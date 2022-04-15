17271885c693d902914674b9dfaddeeae4e6b5c4
Arjun Sivaraman <arjun140702@gmail.com>
2022-04-16 01:30:47 +0530

    Update to Heroku for Flask deployment

--- src/pages/api/model.js
+++ src/pages/api/model.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default async function handler(req, res) {
     if (req.method === "POST") {
         const data = await axios.post(
-            `http://1bf8-136-233-9-107.ngrok.io/`,
+            `https://sportlight-api.herokuapp.com/`,
             req.body.data
         )
         res.send(data.data)
