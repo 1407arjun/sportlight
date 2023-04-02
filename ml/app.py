5113bb2c32f981fa09933805361c3c42707b9908
Arjun Sivaraman <arjun140702@gmail.com>
2023-04-02 23:32:52 +0530

    Update ports and endpoints

--- ml/app.py
+++ ml/app.py
@@ -21,89 +21,83 @@ def return_timestamp():
 
     positive = list()
 
-    add=["Straight","bat","ball","biggie","Cover","OnDrive","Square","Forward","stadium","Defence","Sweep","Reverse",
-    "FrontFoot ","LegGlance ","BackFoot","SquareCut","Pull ","Shot","Hook","Uppercut","Cut","Helicopter ","SwitchHit",
-    "Dilscoop","class","bounce","Upper","Uppish","Scoop ","Inside","Out","Shots","Bouncer", "Outswinger","Inswinger",
-    "ReverseSwing","played","LegCutter","OffCutter","Yorker","Slower","Spin","LegBreak ","OffBreak","Googly ",
-    "Doosra","Topspin ","CarromBall","Slider","ArmBall","Infield","InnerRing","Outfield","Catching","Wicketkeeper",
-    "Slip","Gully","LegSlip","LegGully","Sillypoint","Sillymidoff","Shortleg","Sillymidon","InnerRing","Point","BackwardPoint",
-    "MidOff","Cover","MidOn","SquareLeg","Backward ","SquareLeg","MidWicket","FineLeg","Outfield","ThirdMan",
-    "DeepPoint","BackwardPoint","ExtraCover","LongOff","FineLeg","LongLeg","LongOn","Deep","Cover","played","account"
-    "cricket","hard","sides","man","finishes","one","crucial","Captain","shot","six","four","boundary","line","drive",
-    "celebrate","placement","beauty","fifty","century","perfect","magnifcient","world","cup","batting","fielding","bowling",
-    "catch","caught","out","stumped","one","bowled","night","final","room","taken","edged","wicket","review","DRS","cuts","out","short"]
+    add = ["Straight", "bat", "ball", "biggie", "Cover", "OnDrive", "Square", "Forward", "stadium", "Defence", "Sweep", "Reverse",
+           "FrontFoot ", "LegGlance ", "BackFoot", "SquareCut", "Pull ", "Shot", "Hook", "Uppercut", "Cut", "Helicopter ", "SwitchHit",
+           "Dilscoop", "class", "bounce", "Upper", "Uppish", "Scoop ", "Inside", "Out", "Shots", "Bouncer", "Outswinger", "Inswinger",
+           "ReverseSwing", "played", "LegCutter", "OffCutter", "Yorker", "Slower", "Spin", "LegBreak ", "OffBreak", "Googly ",
+           "Doosra", "Topspin ", "CarromBall", "Slider", "ArmBall", "Infield", "InnerRing", "Outfield", "Catching", "Wicketkeeper",
+           "Slip", "Gully", "LegSlip", "LegGully", "Sillypoint", "Sillymidoff", "Shortleg", "Sillymidon", "InnerRing", "Point", "BackwardPoint",
+           "MidOff", "Cover", "MidOn", "SquareLeg", "Backward ", "SquareLeg", "MidWicket", "FineLeg", "Outfield", "ThirdMan",
+           "DeepPoint", "BackwardPoint", "ExtraCover", "LongOff", "FineLeg", "LongLeg", "LongOn", "Deep", "Cover", "played", "account"
+           "cricket", "hard", "sides", "man", "finishes", "one", "crucial", "Captain", "shot", "six", "four", "boundary", "line", "drive",
+           "celebrate", "placement", "beauty", "fifty", "century", "perfect", "magnifcient", "world", "cup", "batting", "fielding", "bowling",
+           "catch", "caught", "out", "stumped", "one", "bowled", "night", "final", "room", "taken", "edged", "wicket", "review", "DRS", "cuts", "out", "short"]
 
     for i in add:
         for synset in wordnet.synsets(i):
             for lemma in synset.lemmas():
-                positive.append(lemma.name()) 
+                positive.append(lemma.name())
 
+    strings = ' '.join(positive)
 
-    strings=' '.join(positive)
-
-
-    l=d['messages']
-    phrases=[]
-    data=[]
-    starttime=[]
+    l = d['messages']
+    phrases = []
+    data = []
+    starttime = []
     for i in l:
         data.append(list(i.values()))
-
-
 
     for i in range(len(data)):
         phrases.append(data[i][1])
         starttime.append(data[i][5])
 
-
     def create_dataframe(matrix, tokens):
         doc_names = [f'doc_{i+1}' for i, _ in enumerate(matrix)]
         df = pd.DataFrame(data=matrix, index=doc_names, columns=tokens)
-        return(df)
+        return (df)
 
-
-    ans=[]
+    ans = []
     for i in phrases:
-        data=[i,strings]
+        data = [i, strings]
         Tfidf_vect = TfidfVectorizer()
         vector_matrix = Tfidf_vect.fit_transform(data)
-        tokens = Tfidf_vect.get_feature_names()
-        create_dataframe(vector_matrix.toarray(),tokens)
+        tokens = Tfidf_vect.get_feature_names_out()
+        create_dataframe(vector_matrix.toarray(), tokens)
         cosine_similarity_matrix = cosine_similarity(vector_matrix)
-        result=create_dataframe(cosine_similarity_matrix,['Phrase','Strings'])
+        result = create_dataframe(
+            cosine_similarity_matrix, ['Phrase', 'Strings'])
         ans.append(result['Phrase'].values[1])
 
-
-    stack_first=[]
-    stack_last=[]
+    stack_first = []
+    stack_last = []
     for i in ans:
-        if(i>=0.00100000000000):
-            index=ans.index(i)
+        if (i >= 0.00100000000000):
+            index = ans.index(i)
             stack_first.append(starttime[index])
 
-
     stack_first.sort()
-    result=[]
+    result = []
     for i in stack_first:
-        if(i not in result):
+        if (i not in result):
             result.append(i)
 
-
-    values=list()
+    values = list()
     for i in range(len(result)-1):
-        if(result[i]+7<result[i+1]):
+        if (result[i]+7 < result[i+1]):
             values.append(result[i])
-            if(result[i+1] not in values):
+            if (result[i+1] not in values):
                 values.append(result[i+1])
 
-
-    res=list()
+    res = list()
     for i in range(len(result)-1):
-        if(result[i]+8<result[i+1]):
+        if (result[i]+8 < result[i+1]):
             res.append(result[i])
-            if(result[i+1] not in values) and (result[i] not in values):
+            if (result[i+1] not in values) and (result[i] not in values):
                 res.append(result[i+1])
 
-
-    data={"keys":res}
+    data = {"keys": res}
     return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
--- src/pages/api/model.js
+++ src/pages/api/model.js
@@ -2,10 +2,15 @@ import axios from "axios"
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
-        const data = await axios.post(
-            `https://sportlight.onrender.com/`,
-            req.body.data
-        )
-        res.send(data.data)
+        console.log(req.body.data)
+        try {
+            const data = await axios.post(
+                `http://172.17.47.236:8080/`,
+                req.body.data
+            )
+            res.send(data.data)
+        } catch (e) {
+            console.log(e)
+        }
     }
 }
